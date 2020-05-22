bbe01d292749c3b108dd26e6185fe6d9471c3963
Anshita00 <anshitaggarwal2000@gmail.com>
2020-05-22 12:46:35 +1000

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const date = new Date();
+  const time = date.getHours() + " " + date.getMinutes();
+  const [showTimer, setTimer] = useState();
+
+  function changeTimer(val) {
+    setTimer(val);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { showTimer
+        ? <div> {time} </div> 
+        : <div>
+            Select duration screen
+         </div> 
+    }
     </div>
   );
 }
