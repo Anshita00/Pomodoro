e25bdb070defa30169444db4d1190d7af88b7b79
Anshita00 <anshitaggarwal2000@gmail.com>
2020-06-03 01:33:54 +1000

    Added a basic timer for study mins

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -4,24 +4,38 @@ import './App.css';
 function Timer() {
 
   const initialTime = {
-    hours: 5,
     minutes: 25,
+    seconds: 0
   };
+
   const [time, setTime] = useState(initialTime);
 
-  function changeTime(value, name) {
-    console.log(time);
-    setTime(function(prev) {
-      return {
-        ...prev,
-        [name]: value
-      }
+  function changeTime() {
+    let newSeconds = time.seconds;
+    let newMinutes = time.minutes;
+    if (time.seconds == 0) {
+      newSeconds = 59;
+      newMinutes -= 1;
+    } else {
+      newSeconds -= 1;
+    }
+
+  clearInterval(interval);
+    setTime({
+        minutes: newMinutes,
+        seconds: newSeconds
     });
   } 
 
 
+  let interval = setInterval(changeTime, "1000");
+
   return (
-      <div> Show timer </div>
+      <div>
+        <span class="timer">
+          {time.minutes} {time.seconds}
+        </span>
+      </div>
   );
 }
 
